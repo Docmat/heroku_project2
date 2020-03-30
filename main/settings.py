7d773e4f4f89ff48fb55a71a18f6d991d8dae518
Docmat <asanalieveldiyar@gmail.com>
2020-03-30 23:56:39 +0600

    new updates

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cw*4osi5m@+m+8moj$8$t5oj6as856z%&1=5+2yh@ja8j!$&4='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
